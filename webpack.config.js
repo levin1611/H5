2e9a7e68e24435dd00c2766001cc38c6299e1c29
‘jiangyuzhuang <jiangyuzhuang1989@163.com>
2019-01-16 15:35:12 +0800

    压缩打包

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,36 @@ var webpack = require('webpack');
 var path =require('path')
 var HtmlwebpackPlugin = require("html-webpack-plugin");
 var CleanWebpackPlugin = require("clean-webpack-plugin");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 module.exports = {
     mode:'production',
     entry:{
         index: './js/index.js'
     },
     output: {
-        path: __dirname+'/bundle',
-        filename: 'bundle.js'
+        path:path.resolve(__dirname,"bundle"),
+        filename:"js/[name].js"
     },
+   optimization: {
+    splitChunks: {
+      cacheGroups: {
+        
+      },
+    },
+  },
     module: {
         rules:[
             { 
               test: /\.css$/, 
               use: [
-                "style-loader",
+                {
+                    loader:MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: '../'
+                    }
+                },
+                //MiniCssExtractPlugin.loader,
                 "css-loader"
               ]
             },
@@ -27,9 +42,9 @@ module.exports = {
                         loader: "file-loader",
                         options: {
                             limit:5000,   //大于50K的 都打包
-                            name:"[hash:8].[name].[ext]",
-                            publicPath:"./images",  //替换CSS引用的图片路径 可以替换成爱拍云上的路径
-                            outputPath:"./images"        //生成之后存放的路径
+                            name:"images/[hash:8].[name].[ext]",
+                            //publicPath:"./images",  //替换CSS引用的图片路径 可以替换成爱拍云上的路径
+                            //outputPath:"./images"        //生成之后存放的路径
                         }
                     }
                    
@@ -58,6 +73,10 @@ module.exports = {
                 removeAttributeQuotes:true
             },
             hash:true
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+        }),
+        new OptimizeCSSAssetsPlugin(),
     ]
 };
